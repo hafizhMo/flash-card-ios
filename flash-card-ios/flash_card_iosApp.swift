18ed218b9497e6f695c09d8abf80ab471f3cd622
hafizhMo <hafizh.hizb@gmail.com>
2024-06-11 15:56:42 +0700

    init project

---
+++ flash-card-ios/flash_card_iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  flash_card_iosApp.swift
+//  flash-card-ios
+//
+//  Created by Hafizh Mo on 11/06/24.
+//
+
+import SwiftUI
+
+@main
+struct flash_card_iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
