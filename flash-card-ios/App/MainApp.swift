be39899b194e619a623f2d0e37e223f6729f6f9f
hafizhMo <hafizh.hizb@gmail.com>
2024-06-27 14:51:15 +0700

    reset project

--- flash-card-ios/App/MainApp.swift
+++ flash-card-ios/App/MainApp.swift
@@ -6,34 +6,13 @@
 //
 
 import SwiftUI
-import SwiftData
 
 @main
 struct MainApp: App {
-  @ObservedObject var router = Router()
   
   var body: some Scene {
     WindowGroup {
-      NavigationStack(path: $router.navPath) {
-        PlayingView()
-          .navigationDestination(for: Router.Destination.self) { destination in
-            switch destination {
-            case .editCard:
-              EditCardView()
-            case .editDeck:
-              EditDeckView()
-            case .home:
-              HomeView()
-            case .playing:
-//              PlayingView()
-              ListDeckView()
-            case .listDeck:
-              ListDeckView()
-            }
-          }
-      }
-      .environmentObject(router)
-      .modelContainer(for: Deck.self)
+      MainScreen()
     }
   }
 }
--- flash-card-ios/App/MainScreen.swift
+++ flash-card-ios/App/MainScreen.swift
@@ -1,18 +1,18 @@
 //
-//  HomeView.swift
+//  MainView.swift
 //  flash-card-ios
 //
-//  Created by Hafizh Mo on 11/06/24.
+//  Created by Hafizh Mo on 27/06/24.
 //
 
 import SwiftUI
 
-struct HomeView: View {
+struct MainScreen: View {
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
     }
 }
 
 #Preview {
-    HomeView()
+    MainScreen()
 }
